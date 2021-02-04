966329876281b2a525c49219c9cc270c27a7ef77
Matt Goodman <matt@Matts-iMac.local>
2021-02-03 21:34:08 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,83 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Modal
+} from 'react-native';
+
+
+import Login from './components/login';
+import LoginError from './components/login-error';
+import Registration from './components/registration';
+import RegistrationSuccess from './components/registration-success';
+import Tasks from './components/tasks';
+
+const App: () => React$Node = () => {
+  //set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+  const [errorModalVisible, setErrorModalVisible] = useState(false);
+  const [errorMessageText, setErrorMessageText] = useState('');
+  const [currentScreen, setCurrentScreen] = useState('login');
+
+  const logoutUser = () => {
+    auth()
+      .signOut()
+      .then(() => setCurrentScreen('login'));
+  };
+
+  const hideError = () => {
+    setErrorModalVisible(false);
+  };
+
+  const switchScreen = screen => {
+    setCurrentScreen(screen);
+  };
+
+  if (initializing) {
+    return null;
+  }
+
+  if (!user) {
+    return (
+      <>
+        <StatusBar barStyle="light-content" />
+        <SafeAreaView>
+          <Modal animationType='slide' transparent={true} visible={errorModalVisible} onRequestClose={() => hideError()}>
+            <LoginError dismissError={hideError} ErrorMessage={errorMessageText} />
+          </Modal>
+          {currentScreen === 'login' ? <Login doLogin={doLogin} switchScreen={switchScreen} /> : null}
+          {currentScreen === 'register' ? <Registration switchScreen={switchScreen} doRegister={doRegister} /> : null}
+        </SafeAreaView>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView>
+        {currentScreen === 'registration-success' ? <RegistrationSuccess switchScreen={switchScreen} /> : null}
+        {currentScreen === 'tasks' ? <Tasks logoutUser={logoutUser} User={user} /> : null}
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
